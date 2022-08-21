eb2ba188b0c1e351d14414b14a3ba28278ebc01f
John Corrigan <jcorrigan322@gmail.com>
2022-08-21 17:09:28 -0400

    added win conditions

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const player = () => {
+
+}
+
+
+
+
+
 
 
 const gameBoard = (() => {
@@ -16,15 +24,19 @@ gameBoard.setRender(gameBoard.game)
 
 
 let currentGame = [];
+let playerxPicks = [];
+let playeroPicks = [];
 let playerX = true;
 let playerO = false;
 
 //listens for click on grid-item
 document.querySelectorAll('.grid-item').forEach(item => {
     item.addEventListener('click', function(){
+        let current = item.dataset.board;
         if(playerX === true && item.textContent === ''){
             item.textContent = 'X';
             currentGame.push('X')
+            playerxPicks.push(current)
             //gameBoard.setRender(newGame)
             playerX = false;
             playerO = true;
@@ -33,6 +45,7 @@ document.querySelectorAll('.grid-item').forEach(item => {
         if(playerO === true && item.textContent === ''){
             item.textContent = 'O';
             currentGame.push('O')
+            playeroPicks.push(current)
             //gameBoard.setRender(newGame);
             playerO = false;
             playerX = true;
@@ -47,9 +60,25 @@ document.querySelector('.restart').addEventListener('click', function(){
     //clears board
     gameBoard.setRender(gameBoard.game);
     currentGame = [];
+    playeroPicks = [];
+    playerxPicks = [];
     playerX = true;
     playerO = false;
-})
+});
+
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+
 
 
 
