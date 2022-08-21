4f3c91e857b18facd928076efe469921c36b1734
John Corrigan <johncorrigan@Johns-MacBook-Pro.local>
2022-08-21 11:02:09 -0400

    minimal js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const container = document.getElementById("container")
+const board = document.getElementById("game-board")
+let game = ['X', 'O', 'X', 'O']
 
+const gameBoard = () => {
+    let game = ['X', 'O', 'X', 'O']
 
+    function render(game){
+        for(i = 0; i < game.length; i++){
+            let current = document.querySelector('[data-board="${i}"]')
+            
+        }
+    }
+}
+
+function render(game){
+    for(i = 0; i < game.length; i++){
+        let current = document.querySelector(`[data-board='${i}']`)
+        current.textContent = game[i];
+    }
+}
+
+render(game)
 
 
 
