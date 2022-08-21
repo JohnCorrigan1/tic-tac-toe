81b986b4120289703d62902317f4dd26f8192576
John Corrigan <jcorrigan322@gmail.com>
2022-08-21 16:16:41 -0400

    lots of functionality

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-const container = document.getElementById("container")
-const board = document.getElementById("game-board")
-let game = ['X', 'O', 'X', 'O']
 
-const gameBoard = () => {
-    let game = ['X', 'O', 'X', 'O']
 
-    function render(game){
+const gameBoard = (() => {
+    const game = ['', '', '', '', '', '', '', '', ''];
+       
+    function setRender(game){
         for(i = 0; i < game.length; i++){
-            let current = document.querySelector('[data-board="${i}"]')
-            
+            let current = document.querySelector(`[data-board='${i}']`);
+            current.textContent = game[i];
         }
     }
-}
-
-function render(game){
-    for(i = 0; i < game.length; i++){
-        let current = document.querySelector(`[data-board='${i}']`)
-        current.textContent = game[i];
-    }
-}
-
-render(game)
+    return {setRender, game}
+})();
+//this clears board or intializes board on open
+gameBoard.setRender(gameBoard.game)
 
 
+let currentGame = [];
+let playerX = true;
+let playerO = false;
+
+//listens for click on grid-item
+document.querySelectorAll('.grid-item').forEach(item => {
+    item.addEventListener('click', function(){
+        if(playerX === true && item.textContent === ''){
+            item.textContent = 'X';
+            currentGame.push('X')
+            //gameBoard.setRender(newGame)
+            playerX = false;
+            playerO = true;
+        }
+        
+        if(playerO === true && item.textContent === ''){
+            item.textContent = 'O';
+            currentGame.push('O')
+            //gameBoard.setRender(newGame);
+            playerO = false;
+            playerX = true;
+        }
+
+        
+    })
+})
 
 
+document.querySelector('.restart').addEventListener('click', function(){
+    //clears board
+    gameBoard.setRender(gameBoard.game);
+    currentGame = [];
+    playerX = true;
+    playerO = false;
+})
+
+
+
+
+// gridItem.addEventlisener('click', function(){
+//     console.log("test")
+//     return
+// });
